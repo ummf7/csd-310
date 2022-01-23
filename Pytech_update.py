7bf91765b30104c1ddfeae6be82383e94a3d8aa7
ummf7 <95514396+ummf7@users.noreply.github.com>
2022-01-23 15:05:22 -0500

    Create Pytech_update.py

---
+++ Pytech_update.py
@@ -0,0 +1,15 @@
+ doc = db.collection_name.find_one({“student_id”: “1007”}) 
+“first_name”: “Thorin” 
+  {“$set”: {“last_name”: “Fields”}})
+  
+doc = db.collection_name.find_
+one({“student_id”: “1008”})
+“first_name”: “Billbo"  {“$set”: {“last_name”: “Stone”}})
+  
+doc = db.collection_name.find_one({“student_id”: “1009”})
+“first_name”: “Frodo”
+  {“$set”: {“last_name”: “”}})Bower
+  
+
+
+
