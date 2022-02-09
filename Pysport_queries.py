54694b9598054c5563c3b9cfa63442dfdb0d3339
MMFields <95514396+ummf7@users.noreply.github.com>
2022-02-08 21:54:05 -0500

    Create Pysport_queries.py

---
+++ Pysport_queries.py
@@ -0,0 +1,12 @@
+
+--cursor = db.cursor()
+
+team_id, team_name, mascot FROM team;
+cursor.execute(“SELECT team_id,
+               
+ teams = cursor.fetchall()
+               
+ print(“Team Name: {}”.format(team[1]))
+             
+
+
